141d224fdb0d48743ca4ca0d2775b90851b9f449
apredsa <apredsa@gmail.com>
2017-03-31 12:59:04 +0200

    added for loop example

--- includes/example_01.php
+++ includes/example_01.php
@@ -1,5 +1,15 @@
 <?php
 
-$colors = [];
+$colors = ['blue', 'green', 'red', 'black', ];
+
+for($a = 0; $a < count($colors); $a++) {    
+    for($b = 0; $b < count($colors); $b++) {       
+
+    echo '<ul>';
+    echo '<li class="' . $colors[$b] . '"> ' . $colors[$b]. ' </li>';
+    echo '</ul>';
+
+    }
+}
 
 ?>
